1b4fba13f14b0b6770b060081c3d0d1c3ea4a1c6
Shashank Kotyan <shashankkotyan@gmail.com>
2020-06-05 01:09:02 +0530

    Update

--- run_unit_tests.sh
+++ run_unit_tests.sh
@@ -1,30 +1,30 @@
 #!/usr/bin/env bash
 
-# Check Models
-for attack in fglinf
-do
-    for family in 0
-    do
-        for dataset in 0 
-        do
-            for model in 0 1
-            do
-                python -u code/run_attack.py $attack -f $family -d $dataset -m $model --samples 1 --epochs 2 -v 
-            done
-        done
-    done
+# # Check Models
+# for attack in fglinf
+# do
+#     for family in 0
+#     do
+#         for dataset in 0 
+#         do
+#             for model in 0 1
+#             do
+#                 python -u code/run_attack.py $attack -f $family -d $dataset -m $model --samples 1 --epochs 2 -v 
+#             done
+#         done
+#     done
 
-    for family in 1
-    do
-        for dataset in 0
-        do
-            for model in 1 2 3 4 5 6 7
-            do
-                python -u code/run_attack.py $attack -f $family -d $dataset -m $model --samples 1 --epochs 2 -v
-            done
-        done
-    done
-done
+#     for family in 1
+#     do
+#         for dataset in 0
+#         do
+#             for model in 1 2 3 4 5 6 7
+#             do
+#                 python -u code/run_attack.py $attack -f $family -d $dataset -m $model --samples 1 --epochs 2 -v
+#             done
+#         done
+#     done
+# done
 
 #Check Our Attacks
 for attack in pixel threshold 
@@ -55,28 +55,28 @@ do
     done
 done
 
-#Check Other Attacks
-for attack in bilinf pgdlinf deep newton
-do
-        for family in 0
-        do
-            for dataset in 0
-            do
-                for model in 0
-                do
-                    python -u code/run_attack.py $attack -f $family -d $dataset -m $model --samples 1 --epochs 2 -v 
-                done
-            done
-        done
+# #Check Other Attacks
+# for attack in bilinf pgdlinf deep newton
+# do
+#         for family in 0
+#         do
+#             for dataset in 0
+#             do
+#                 for model in 0
+#                 do
+#                     python -u code/run_attack.py $attack -f $family -d $dataset -m $model --samples 1 --epochs 2 -v 
+#                 done
+#             done
+#         done
 
-        for family in 1
-        do
-            for dataset in 0
-            do
-                for model in 1
-                do
-                    python -u code/run_attack.py $attack -f $family -d $dataset -m $model --samples 1 --epochs 2 -v
-                done
-            done
-        done
-done
+#         for family in 1
+#         do
+#             for dataset in 0
+#             do
+#                 for model in 1
+#                 do
+#                     python -u code/run_attack.py $attack -f $family -d $dataset -m $model --samples 1 --epochs 2 -v
+#                 done
+#             done
+#         done
+# done
